ce11b363f61578966bf6a9b54359012ea16ae324
whispxr <miguel.obanos.gonzalez@gmail.com>
2025-01-17 20:34:45 -0300

    Función de choque
    siente el choque, choque, choque , choque

--- mainsimulation.py
+++ mainsimulation.py
@@ -1,14 +1,14 @@
 import os
 import pygame
+import random
+import sys
 clear = lambda: os.system('cls')
 
-import pygame
-import sys
 
 class Pelota:
     def __init__(self, radio, masa, velocidad, x, y, color):
         self.radio = radio
-        self.masa = masa
+        self.masa = masa #todavia sin uso xd
         self.velocidad = velocidad  
         self.x = x
         self.y = y
@@ -21,12 +21,10 @@ class Pelota:
     def actualizar(self, size):
         # vertical
         if self.y + self.radio > size[1] or self.y - self.radio < 0:
-            self.radio -= 1
             self.velocidad[1] = -self.velocidad[1]  #se invierte la direccion
 
         #horizontal
         if self.x + self.radio > size[0] or self.x - self.radio < 0:
-            self.radio -= 1
             self.velocidad[0] = -self.velocidad[0]  
 
         # Actualizar posición
@@ -34,21 +32,33 @@ class Pelota:
         self.y += self.velocidad[1]
 
 
+def checkcollision(pelota1, pelota2):
+    distx = pelota1.x - pelota2.x
+    disty = pelota1.y - pelota2.y
+    distancia = (distx ** 2 + disty ** 2) ** 0.5 # distancia por pitagoras
 
-    def checkcollision(self):
-        pass
+    if distancia <= pelota1.radio + pelota2.radio: 
+        # se transfiere la energia de la otra pelota
+        pelota1.velocidad[0], pelota2.velocidad[0] = pelota2.velocidad[0], pelota1.velocidad[0]
+        pelota1.velocidad[1], pelota2.velocidad[1] = pelota2.velocidad[1], pelota1.velocidad[1]
+
 
 # Main loop
 def main():
     pygame.init()
 
-    ancho, alto = 800, 720
+    ancho, alto = 1400, 720
     pantalla = pygame.display.set_mode((ancho, alto))
     pygame.display.set_caption("Pelota con Rebote")
 
 
-    pelota = Pelota(20, 1, [4, 4], 400, 300,(255,0,0))
-    pelota2 = Pelota(40, 1, [5,5], 400, 300,(0,255,0))
+    pelota1 = Pelota(20, 1, [4, 4], 200,400,(255,0,0))
+    pelota2 = Pelota(30, 1, [7,7], 300,500,(0,255,0))
+    pelota3 = Pelota(40, 1, [3,3], 500,600,(0,0,255))
+
+
+
+    pelotas = [pelota1,pelota2,pelota3]
 
 
     while True:
@@ -58,16 +68,27 @@ def main():
                 sys.exit()
         pantalla.fill((0, 0, 0))  
 
-        pelota.ShowPelota(pantalla)  
-        pelota2.ShowPelota(pantalla)
+        for i in pelotas:
+            i.ShowPelota(pantalla)
 
 
-        pelota.checkcollision()
-        pelota2.checkcollision()
 
-        pelota.actualizar((ancho, alto))
-        pelota2.actualizar((ancho,alto))
 
+
+        checkcollision(pelota1,pelota2)
+        checkcollision(pelota1,pelota3)
+        checkcollision(pelota2,pelota3)
+
+        # for i in pelotas:
+        #     for j in pelotas:
+        #         if i == j:
+        #             pass
+        #         else:
+        #             checkcollision(i,j)
+        
+
+        for i in pelotas:
+            i.actualizar((ancho,alto))
 
 
 
---
+++ test.py
@@ -0,0 +1,8 @@
+numeros = [1,2,3,4,5,6,7,8]
+
+for i in numeros:
+    for j in numeros:
+        if i == j:
+            pass
+        else:
+            print(i,j)
